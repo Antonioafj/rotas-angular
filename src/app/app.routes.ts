7b995fdce71b2ea447606393973dfd7ab9f4e8a9
Antonioafj <antonioafj.edu@gmail.com>
2024-11-07 09:14:17 -0400

    criadas rotas e child

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { TitleComponent } from './pages/index/title/title.component';
+import { CardComponent } from './pages/portifolio/card/card.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component: TitleComponent, pathMatch:'full'},
+    {path:'portifolio', component: CardComponent, children:[
+        {path: ':id', component: CardComponent},
+        {path: ':id/:token', component: CardComponent},
+    ]},
+    {path:'**', redirectTo:''}
+];
